e0e9bff5dab747ee834f5aab2768f076e141a38d
leayajoseph <leaya231@gmail.com>
2024-03-13 09:37:59 +0530

    reset_password created

--- lib/features/auth/screens/auth_screen.dart
+++ lib/features/auth/screens/auth_screen.dart
@@ -40,92 +40,94 @@ class _AuthScreenState extends State<AuthScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        alignment: Alignment.center,
-        padding: EdgeInsets.all(20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SvgPicture.asset("assets/img1.svg",width: 300,height: 300, alignment: Alignment.center,),
-            SizedBox(height: 10,),
-            Text("Login",style: TextStyle(
-              fontSize: 22,
-              fontWeight: FontWeight.w500
-            ),),
-            SizedBox(height: 10,),
-            SizedBox(
-              width: 400,
-              child: TextField(
-                controller: ob1,
-               decoration: InputDecoration(
-                 prefixIcon: Icon(Icons.email_outlined),
-                   border: OutlineInputBorder(),
-                   hintText: 'email',
-                   labelText: '',
-                   fillColor: Colors.grey,
-                   focusedBorder: OutlineInputBorder(
-                     borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                     borderSide: const BorderSide(
-                       color: Colors.black,
-                       width: 2,
-                     ),
-                   )
-               ),
-              ),
-            ),
-            SizedBox(height: 20,),
-            SizedBox(
-              width: 400,
-              child: TextField(
-                controller: ob2,
-                obscureText: true,
-                decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.lock_outline),
-                    border: OutlineInputBorder(),
-                    hintText: 'password',
-                    labelText: '',
-                    fillColor: Colors.grey,
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                      borderSide: const BorderSide(
-                        color: Colors.black,
-                        width: 2,
-                      ),
-                    )
+      body: SingleChildScrollView(
+        child: Container(
+          alignment: Alignment.center,
+          padding: EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SvgPicture.asset("assets/img1.svg",width: 300,height: 300, alignment: Alignment.center,),
+              SizedBox(height: 10,),
+              Text("Login",style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.w500
+              ),),
+              SizedBox(height: 10,),
+              SizedBox(
+                width: 400,
+                child: TextField(
+                  controller: ob1,
+                 decoration: InputDecoration(
+                   prefixIcon: Icon(Icons.email_outlined),
+                     border: OutlineInputBorder(),
+                     hintText: 'email',
+                     labelText: '',
+                     fillColor: Colors.grey,
+                     focusedBorder: OutlineInputBorder(
+                       borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                       borderSide: const BorderSide(
+                         color: Colors.black,
+                         width: 2,
+                       ),
+                     )
+                 ),
                 ),
               ),
-            ),
-            SizedBox(height: 20,),
-            GestureDetector(
-              child: Text("Forget password?"),
-              onTap: (){
-                Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=>ForgetPassword()));
-              },
-            ),
-            SizedBox(height: 30,),
-            SizedBox(
-              width: 200,
-              child: ElevatedButton(onPressed:login,
-                child: Text("Continue"),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.teal,
-                foregroundColor: Colors.white,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20)
-                )
-
-              ),),
-            ),
-            SizedBox(height: 20,),
-            GestureDetector(
-              child: Text("Dont have an account? signup"),
-              onTap: ()
-              {
-                Navigator.push(context, MaterialPageRoute(builder: (context)=>SignUp()));
-              },
-            )
-
-          ],
+              SizedBox(height: 20,),
+              SizedBox(
+                width: 400,
+                child: TextField(
+                  controller: ob2,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.lock_outline),
+                      border: OutlineInputBorder(),
+                      hintText: 'password',
+                      labelText: '',
+                      fillColor: Colors.grey,
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                        borderSide: const BorderSide(
+                          color: Colors.black,
+                          width: 2,
+                        ),
+                      )
+                  ),
+                ),
+              ),
+              SizedBox(height: 20,),
+              GestureDetector(
+                child: Text("Forget password?"),
+                onTap: (){
+                  Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=>ForgetPassword()));
+                },
+              ),
+              SizedBox(height: 30,),
+              SizedBox(
+                width: 200,
+                child: ElevatedButton(onPressed:login,
+                  child: Text("Continue"),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.teal,
+                  foregroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)
+                  )
+        
+                ),),
+              ),
+              SizedBox(height: 20,),
+              GestureDetector(
+                child: Text("Dont have an account? signup"),
+                onTap: ()
+                {
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=>SignUp()));
+                },
+              )
+        
+            ],
+          ),
         ),
       ),
     );
